c5afbbeb757d0da39c4c18a6f72e2863778a6c9d
Shekinah Glory <sfu@deboing>
2022-05-12 16:22:25 -0400

    created a dev class

---
+++ Dev.java
@@ -0,0 +1,26 @@
+
+
+public class Dev {
+                  
+        public Dev(){
+
+
+
+	}
+
+	public static void main(String [] args){
+	                                                             
+		System.out.println("Developing something great");
+		
+		Dev dev = new Dev();
+
+		System.out.println(dev.result());
+	}
+
+
+	public String result(){
+		return "Reality is great";
+	}
+
+}
+
